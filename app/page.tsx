7d3e8caa45c6a7da11dc308e3f90392e25eb5746
Giang Nguyen <giangnguyen221295@gmail.com>
2024-05-08 17:14:34 +0700

    modified UI input url page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
-        <Card className='w-[640px]'>
+        <Card className='w-[640px] shadow-lg'>
           <CardHeader className='pb-5'>
             <CardTitle className={`${inika.className} text-[32px]`}>Kim Dong's Publication Crawler</CardTitle>
           </CardHeader>
--- components/ScheduleLinkInput.tsx
+++ components/ScheduleLinkInput.tsx
@@ -5,6 +5,8 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { ChangeEvent, useState } from 'react';
 import { scrapeSite } from '@/utils/scrapeSite';
+import { Download, SearchIcon, X } from 'lucide-react';
+import Link from 'next/link';
 
 const ScheduleLinkInput = () => {
   const [data, setData] = useState<Object[]>([]);
@@ -20,25 +22,57 @@ const ScheduleLinkInput = () => {
       //   const url = `http://nxbkimdong.com.vn/blogs/lich-phat-hanh-sach-dinh-ky/lich-phat-hanh-sach-dinh-ki-thang-5-2024`;
       const { results, hasError } = await scrapeSite(inputUrl);
       setData(results);
-      console.log('🚀 ~ handleGetScrapeSite ~ data:', data);
+      console.log('🚀 ~ handleGetScrapeSite ~ data:', results);
       setHasError(hasError);
     } catch (err) {
       console.error(err);
     }
   };
 
+  const handleSearchByName = () => {};
+
+  const handleExport = () => {};
+
   return (
     <>
-      <div className='mb-1.5 text-sm'>Publication schedule link</div>
+      <div className='mb-1.5 text-sm'>{data.length > 0 && !hasError ? 'Search by name' : 'Publication schedule link'}</div>
       <div className='flex w-full space-x-2'>
-        <div className='flex w-full flex-col space-y-1.5'>
-          <Input id='name' placeholder='Enter schedule link here' value={inputUrl} onChange={handleChangeUrl} />
+        <div className='flex flex-1 flex-col space-y-1.5'>
+          <Input
+            id='name'
+            placeholder={data.length > 0 && !hasError ? 'Enter searching name here' : 'Enter schedule link here'}
+            value={inputUrl}
+            onChange={handleChangeUrl}
+          />
         </div>
-        <Button type='submit' onClick={handleGetScrapeSite}>
-          Crawl
-        </Button>
+        {data.length > 0 && !hasError ? (
+          <>
+            <Button onClick={handleSearchByName} className='w-[56px]'>
+              <SearchIcon size={16} />
+            </Button>
+            <Button onClick={handleExport} variant={'ghost'} className='size-10 bg-[#E2E8F0] p-0'>
+              <Download size={16} />
+            </Button>
+          </>
+        ) : (
+          <Button type='submit' onClick={handleGetScrapeSite}>
+            Crawl
+          </Button>
+        )}
       </div>
       {hasError ? <div className='mt-[10px] text-xs text-[#EB5757]'>Invalid link. Head to nxbkimdong.com.vn to get the correct link.</div> : null}
+
+      <div className='mt-6 flex items-baseline gap-2'>
+        <span className='text-sm font-medium'>Link:</span>
+        <div className='flex min-w-0 flex-1 items-center'>
+          <Link href={inputUrl} className='flex-1 truncate text-xs text-[#475569] underline hover:text-blue-500'>
+            {inputUrl}
+          </Link>
+          <div className='flex size-4 cursor-pointer justify-center rounded-full border-2 bg-[#E2E8F0]'>
+            <X size={12} />
+          </div>
+        </div>
+      </div>
     </>
   );
 };

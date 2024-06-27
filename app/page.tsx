802eca0a32395425eea553cc16480adc43784af7
Mai Doan <doantuyetmai2000@gmail.com>
2024-06-27 10:45:04 +0700

    fix: update max width

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import PublicationCrawler from '@/components/PublicationCrawler';
 export default async function Home() {
   return (
     <main className='flex min-h-screen w-full flex-col items-center justify-between p-4 lg:p-24'>
-      <Card className='w-full max-w-[640px] shadow-lg'>
+      <Card className='w-full max-w-screen-md shadow-lg'>
         <CardHeader className='pb-5'>
           <CardTitle className='font-inika text-[32px]'>Kim Dong&apos;s Publication Crawler</CardTitle>
         </CardHeader>

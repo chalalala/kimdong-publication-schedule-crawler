ea275e53acdfbec44eca83fa6e70efb410c6ccb0
Mai Doan <doantuyetmai2000@gmail.com>
2024-07-01 14:26:44 +0700

    fix: update query month

--- components/PublicationCrawler.tsx
+++ components/PublicationCrawler.tsx
@@ -7,9 +7,13 @@ import { CrawlData } from '@/types/CrawlData';
 
 const PublicationCrawler = () => {
   const [crawlData, setCrawlData] = useState<CrawlData[] | undefined>();
-  const [url, setUrl] = useState(
-    () => `https://nxbkimdong.com.vn/blogs/lich-phat-hanh-sach-dinh-ky/lich-phat-hanh-sach-dinh-ki-thang-${new Date().getMonth() + 2}-2024`
-  );
+  const [url, setUrl] = useState(() => {
+    const today = new Date().getDate();
+    const thisMonth = new Date().getMonth() + 1;
+    const queryMonth = today > 25 ? thisMonth + 1 : thisMonth;
+
+    return `https://nxbkimdong.com.vn/blogs/lich-phat-hanh-sach-dinh-ky/lich-phat-hanh-sach-dinh-ki-thang-${queryMonth}-2024`;
+  });
 
   return url && crawlData ? (
     <ScheduleData data={crawlData} inputUrl={url} setUrl={setUrl} setCrawlData={setCrawlData} />
